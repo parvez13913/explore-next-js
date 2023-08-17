90c3cedf058b2c5cb7ff5a09450692f972c88ad7
parvez13913 <parvezz13913@gmail.com>
2023-08-17 14:43:43 +0600

    Explore Simple Layout System

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,44 +1,20 @@
-import React from 'react';
-import { Breadcrumb, Button, Layout, Menu, theme } from 'antd';
-import Link from 'next/link';
-
-const { Header, Content, Footer } = Layout;
+import RootLayouts from "../../components/Layouts/RootLayouts";
 
 const HomePage = () => {
-  const {
-    token: { colorBgContainer },
-  } = theme.useToken();
 
   return (
-    <Layout className="layout">
-      <Header style={{ display: 'flex', alignItems: 'center' }}>
-        <div className="demo-logo" />
-        <Menu
-          theme="dark"
-          mode="horizontal"
-          defaultSelectedKeys={['2']}
-        >
-          <Button type='primary'>
-            <Link href='/news'>News</Link>
-          </Button>
-        </Menu>
-      </Header>
-      <Content style={{ padding: '0 50px' }}>
-        <Breadcrumb style={{ margin: '16px 0' }}>
-          <Breadcrumb.Item>Home</Breadcrumb.Item>
-          <Breadcrumb.Item>List</Breadcrumb.Item>
-          <Breadcrumb.Item>App</Breadcrumb.Item>
-        </Breadcrumb>
-        <div className="site-layout-content" style={{
-          background: colorBgContainer,
-          minHeight: "100 vh",
-        }}>
-          Conten
-        </div>
-      </Content>
-      <Footer style={{ textAlign: 'center' }}>Ant Design ©2023 Created by Ant UED</Footer>
-    </Layout>
+    <div>
+      <h1>This is Next.js Home Page</h1>
+    </div>
   );
 };
 
 export default HomePage;
+
+HomePage.getLayout = function getLayout(page) {
+  return (
+    <RootLayouts>
+      {page}
+    </RootLayouts>
+  )
+}
